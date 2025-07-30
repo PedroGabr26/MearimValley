0f2d2771001e1ae2ed13f91037e7975e001df97e
pedro <programmerp4.sntsfrts@gmail.com>
2025-07-30 11:30:23 -0300

    Adicionando vercel.json e preparando para deloy

--- projeto_hub/settings.py
+++ projeto_hub/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2gs+x-_=i)@$21ak9du4^3myk^067^^ijv!^($@0u@qdou0gr3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
