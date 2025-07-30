6b59ebc3f6bf163344a82ff97ca5ac47fb71d981
pedro <programmerp4.sntsfrts@gmail.com>
2025-07-30 19:00:23 -0300

    Adiciona domínio ao ALLOWED_HOSTS

--- projeto_hub/settings.py
+++ projeto_hub/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-2gs+x-_=i)@$21ak9du4^3myk^067^^ijv!^($@0u@qdou0gr3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
