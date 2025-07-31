cbaddbaca8c27ca76f6438d7e721b938192d8c55
pedro <programmerp4.sntsfrts@gmail.com>
2025-07-31 10:07:20 -0300

    Verificacao de models

--- eventos/models.py
+++ eventos/models.py
@@ -4,8 +4,8 @@ class Eventos(models.Model):
     # coloca campo de foto ?
     titulo = models.CharField(max_length=200)
     descricao = models.TextField()
-    local_encontro = models.CharField(max_length=200)
-    data_encontro = models.DateField(help_text='Data para realização do evento')
+    local_evento = models.CharField(max_length=200)
+    data_evento = models.DateField(help_text='Data para realização do evento')
     link = models.URLField(help_text='Link para site de inscrição do Evento')
     data_criacao = models.DateTimeField(auto_now_add=True)
 
@@ -13,4 +13,4 @@ class Eventos(models.Model):
         return self.titulo
 
     class Meta:
-        ordering = ['-data_encontro']
+        ordering = ['-data_evento']
